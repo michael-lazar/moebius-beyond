94dbb69537487aed84b26c4c60761ac946a39181
Michael Lazar <lazar.michael22@gmail.com>
2025-07-24 20:18:42 -0400

    Tweaking rules

--- eslint.config.js
+++ eslint.config.js
@@ -17,29 +17,21 @@ module.exports = [
                 module: 'readonly',
                 exports: 'readonly',
                 global: 'readonly',
-                
-                // Electron globals
-                electron: 'readonly',
-                
+
                 // Browser globals for renderer process
                 window: 'readonly',
                 document: 'readonly',
                 navigator: 'readonly',
                 localStorage: 'readonly',
                 sessionStorage: 'readonly',
-                
-                // jQuery
-                $: 'readonly',
-                jQuery: 'readonly'
             }
         },
         rules: {
             // Disable some rules that may be too strict for legacy code
-            'no-unused-vars': 'warn',
+            'no-unused-vars': 'off',
             'no-undef': 'error',
             'no-console': 'off',
             'semi': ['error', 'always'],
-            'quotes': ['warn', 'double', { allowTemplateLiterals: true }]
         }
     },
     {
